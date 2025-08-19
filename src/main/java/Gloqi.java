0a54e2056064cb82ac50e3e811bde71a96154173
GuoGangQuan <e1232635@u.nus.edu>
2025-08-19 10:05:43 +0800

    Add Gloqi.java
    Let's rename the chatbot to Gloqi, greet, and exit

---
+++ src/main/java/Gloqi.java
@@ -0,0 +1,10 @@
+public class Gloqi {
+
+    public static void main(String[] args) {
+        final String CHATBOT_NAME = "Gloqi";
+        String greetMessage = "Hello I am " + CHATBOT_NAME + "\nhow can i help you";
+        String endMessage = "Bye, see you next time!";
+        System.out.println(greetMessage);
+        System.out.println(endMessage);
+    }
+}
