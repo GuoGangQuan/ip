7bca0cbd3e174d32351b352b5ba09c5672799ee4
GuoGangQuan <e1232635@u.nus.edu>
2025-08-20 10:57:11 +0800

    Add Echo feature

--- src/main/java/Gloqi.java
+++ src/main/java/Gloqi.java
@@ -17,8 +17,19 @@ public class Gloqi {
         System.out.println("_".repeat(50));
     }
 
+    private static String getInput(Scanner scanIn) {
+        System.out.print(">>> ");
+        return scanIn.nextLine();
+    }
+
     public static void main(String[] args) {
         greetMessage();
+        Scanner scanInput = new Scanner(System.in);
+        String cmdInput = getInput(scanInput);
+        while (!cmdInput.equalsIgnoreCase("bye")) {
+            printInPrompt(cmdInput);
+            cmdInput = getInput(scanInput);
+        }
         endMessage();
     }
 }
