74eea6b319488a9d75c3a7ed656aaceefbfd7d51
GuoGangQuan <e1232635@u.nus.edu>
2025-08-27 09:12:21 +0800

    Extract DataManager class to handle load/save tasks

---
+++ src/main/java/DataManager.java
@@ -0,0 +1,75 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DataManager {
+    protected final Path appDataDir;
+    protected final Path appDataFile;
+
+    public DataManager(Path appDataDir, Path appDataFile) {
+        this.appDataDir = appDataDir;
+        this.appDataFile = appDataFile;
+        setupDataFile();
+    }
+
+    private void setupDataFile() {
+        try {
+            if (!Files.exists(appDataDir)) {
+                Files.createDirectories(appDataDir);
+                System.out.println("Storage directory created: " + appDataDir);
+            }
+            if (!Files.exists(appDataFile)) {
+                Files.createFile(appDataFile);
+                System.out.println("Storage file created: " + appDataFile);
+            }
+        } catch (Exception e) {
+            System.out.println("Error during setup: " + e.getMessage());
+        }
+    }
+
+    public void writeDataFile(BankList bankList) {
+        try {
+            Files.write(appDataFile, bankList.SaveBank());
+        } catch (Exception e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+    public BankList loadDataFile() throws GloqiException{
+        BankList bankList = new BankList();
+        try {
+            for (String line : Files.readAllLines(this.appDataFile)) {
+                String[] splitLine = line.split("\\|", 2);
+                int index = Integer.parseInt(splitLine[0]);
+                if (splitLine[1].charAt(index) == '|') {
+                    String taskName = splitLine[1].substring(0, index);
+                    splitLine = splitLine[1].substring(index + 1).split("\\|", 2);
+                    String taskType = splitLine[1].substring(0, 1);
+                    String isDone = splitLine[0];
+                    Task restoreTask;
+                    switch (taskType) {
+                        case "T" -> {
+                            restoreTask = new Todo(taskName);
+                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
+                            bankList.restoreBank(restoreTask);
+                        }
+                        case "D" -> {
+                            restoreTask = new Deadline(new String[]{taskName, splitLine[1].split("\\|", 2)[1]});
+                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
+                            bankList.restoreBank(restoreTask);
+                        }
+                        case "E" -> {
+                            String[] part = splitLine[1].split("\\|", 3);
+                            restoreTask = new Event(new String[]{taskName, part[1], part[2]});
+                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
+                            bankList.restoreBank(restoreTask);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new GloqiException("Failed to load tasks (file might be corrupted): " + e.getMessage());
+        }
+        return bankList;
+
+    }
+}
--- src/main/java/Gloqi.java
+++ src/main/java/Gloqi.java
@@ -26,82 +26,87 @@ public class Gloqi {
         return scanIn.nextLine();
     }
 
-    //this method was inspired by chatGPT
-    private static void setupDataFile(Path appDataDir, Path appDataFile) {
-        try {
-            if (!Files.exists(appDataDir)) {
-                Files.createDirectories(appDataDir); // Creates all missing parent directories
-                System.out.println("Storage directory created: " + appDataDir);
-            }
-            if (!Files.exists(appDataFile)) {
-                Files.createFile(appDataFile); // Creates all missing parent directories
-                System.out.println("Storage file created: " + appDataFile);
-            }
+//    //this method was inspired by chatGPT
+//    private static void setupDataFile(Path appDataDir, Path appDataFile) {
+//        try {
+//            if (!Files.exists(appDataDir)) {
+//                Files.createDirectories(appDataDir); // Creates all missing parent directories
+//                System.out.println("Storage directory created: " + appDataDir);
+//            }
+//            if (!Files.exists(appDataFile)) {
+//                Files.createFile(appDataFile); // Creates all missing parent directories
+//                System.out.println("Storage file created: " + appDataFile);
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+//
+//    }
+//
+//    public static void writeDataFile(Path appDataFile, BankList bankList) {
+//        try {
+//            Files.write(appDataFile, bankList.SaveBank());
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+//    }
 
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-    }
-
-    public static void writeDataFile(Path appDataFile, BankList bankList) {
-        try {
-            Files.write(appDataFile, bankList.SaveBank());
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    public static BankList loadDataFile(Path appDataFile) {
-        BankList bankList = new BankList();
-        try {
-            for (String line : Files.readAllLines(appDataFile)) {
-                String[] splitLine = line.split("\\|", 2);
-                int index = Integer.parseInt(splitLine[0]);
-                if (splitLine[1].charAt(index) == '|') {
-                    String taskName = splitLine[1].substring(0, index);
-                    splitLine = splitLine[1].substring(index + 1).split("\\|", 2);
-                    String taskType = splitLine[1].substring(0, 1);
-                    String isDone = splitLine[0];
-                    Task restoreTask;
-                    switch (taskType) {
-                        case "T" -> {
-                            restoreTask = new Todo(taskName);
-                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
-                            bankList.restoreBank(restoreTask);
-                        }
-                        case "D" -> {
-                            restoreTask = new Deadline(new String[]{taskName, splitLine[1].split("\\|", 2)[1]});
-                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
-                            bankList.restoreBank(restoreTask);
-                        }
-                        case "E" -> {
-                            String[] part = splitLine[1].split("\\|", 3);
-                            restoreTask = new Event(new String[]{taskName, part[1], part[2]});
-                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
-                            bankList.restoreBank(restoreTask);
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("Failed to load tasks (file might be corrupted): " + e.getMessage());
-        }
-        return bankList;
-
-    }
+//    public static BankList loadDataFile(Path appDataFile) {
+//        BankList bankList = new BankList();
+//        try {
+//            for (String line : Files.readAllLines(appDataFile)) {
+//                String[] splitLine = line.split("\\|", 2);
+//                int index = Integer.parseInt(splitLine[0]);
+//                if (splitLine[1].charAt(index) == '|') {
+//                    String taskName = splitLine[1].substring(0, index);
+//                    splitLine = splitLine[1].substring(index + 1).split("\\|", 2);
+//                    String taskType = splitLine[1].substring(0, 1);
+//                    String isDone = splitLine[0];
+//                    Task restoreTask;
+//                    switch (taskType) {
+//                        case "T" -> {
+//                            restoreTask = new Todo(taskName);
+//                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
+//                            bankList.restoreBank(restoreTask);
+//                        }
+//                        case "D" -> {
+//                            restoreTask = new Deadline(new String[]{taskName, splitLine[1].split("\\|", 2)[1]});
+//                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
+//                            bankList.restoreBank(restoreTask);
+//                        }
+//                        case "E" -> {
+//                            String[] part = splitLine[1].split("\\|", 3);
+//                            restoreTask = new Event(new String[]{taskName, part[1], part[2]});
+//                            restoreTask = restoreTask.setMark(("x".equals(isDone)));
+//                            bankList.restoreBank(restoreTask);
+//                        }
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            System.err.println("Failed to load tasks (file might be corrupted): " + e.getMessage());
+//        }
+//        return bankList;
+//
+//    }
 
 
     public static void main(String[] args) {
         Path appDataDir = Paths.get(".", "data");
         Path appDataFile = appDataDir.resolve("data.txt");
-        setupDataFile(appDataDir, appDataFile);
+        DataManager dataManager= new DataManager(appDataDir, appDataFile);
         greetMessage();
         Scanner scanInput = new Scanner(System.in);
         String userInput;
         CommandParser commandParser;
         Command cmd = Command.INVALID;
-        BankList bankList = loadDataFile(appDataFile);
+        BankList bankList = new BankList();
+        try {
+            bankList = dataManager.loadDataFile();
+        } catch (GloqiException e) {
+            printInPrompt(e.getMessage());
+        }
         Task inputTask;
         while (!cmd.equals(Command.BYE)) {
             userInput = getInput(scanInput);
@@ -112,30 +117,30 @@ public class Gloqi {
                     case LIST -> bankList.printList(Gloqi::printInPrompt);
                     case MARK -> {
                         bankList.markTask(commandParser.getIntArg(), Gloqi::printInPrompt);
-                        writeDataFile(appDataFile, bankList);
+                        dataManager.writeDataFile(bankList);
                     }
                     case UNMARK -> {
                         bankList.unmarkTask(commandParser.getIntArg(), Gloqi::printInPrompt);
-                        writeDataFile(appDataFile, bankList);
+                        dataManager.writeDataFile(bankList);
                     }
                     case TODO -> {
                         inputTask = new Todo(commandParser.getStringArg()[0]);
                         bankList.addTask(inputTask, Gloqi::printInPrompt);
-                        writeDataFile(appDataFile, bankList);
+                        dataManager.writeDataFile(bankList);
                     }
                     case DEADLINE -> {
                         inputTask = new Deadline(commandParser.getStringArg());
                         bankList.addTask(inputTask, Gloqi::printInPrompt);
-                        writeDataFile(appDataFile, bankList);
+                        dataManager.writeDataFile(bankList);
                     }
                     case EVENT -> {
                         inputTask = new Event(commandParser.getStringArg());
                         bankList.addTask(inputTask, Gloqi::printInPrompt);
-                        writeDataFile(appDataFile, bankList);
+                        dataManager.writeDataFile(bankList);
                     }
                     case DELETE -> {
                         bankList.deleteTask(commandParser.getIntArg(), Gloqi::printInPrompt);
-                        writeDataFile(appDataFile, bankList);
+                        dataManager.writeDataFile(bankList);
                     }
                     case SHOW -> {
                         bankList.printList(Gloqi::printInPrompt,commandParser.getDateArg());
